a6bdb5feeec393946253f3f84a116bf5ff629249
rebelpis <anhnguyenfx99@gmail.com>
2025-03-12 21:02:26 +0700

    upcode

---
+++ practice11/main2.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+// int main() { cau 2a
+//     int arr[6] = {7, 6, 3, 34, 10, 710}; 
+//     int i = 0 ;
+//     printf("Using for : \n");
+//     for (int i = 0; i < 6; i++) ;
+//      {
+//         printf("The element %d of the array is %d \n", i + 1, arr[i]);
+//         i++ ;
+//     }
+
+//     printf("Using while :\n");
+//     int j = 0;
+//     while (j < 6) {
+//         printf("The element %d of the array is %d \n", j + 1, arr[j]);
+//         j++;
+//     }
+
+//     printf("Using do while :\n");
+//     int k = 0;
+//     do {
+//         printf("The element %d of the array is %d \n", k + 1, arr[k]);
+//         k++;
+//     } while (k < 6);
+
+//     return 0;
+// }
+
+
+// int main() { cau 2b
+//     int arr[6] = {7, 6, 3, 34, 10, 710 }; 
+//     int n = 6;  
+//     int *ptr = arr ;                        
+//     int max = *ptr;      
+         
+//     for(int i = 0; i < n; i++) 
+//     {
+//         if(*(ptr + i) > max) max = *(ptr + i); 
+//     }
+    
+//     printf("Max: %d\n", max);
+    
+//     return 0;
+// }
+
+float giatriy ( float x ){ 
+    float g = 9.8;
+    float y = -0.5*(g*x*x) + x ; 
+    return 0;
+}
+    
+    int main(){
+        int x = 5 ; 
+        float giatriy = float(5); 
+        printf("The position of bullet: yx = %f \n",x);
+        
+    }
+
+
+// int main(){
+// float giatriy;
+// float x ;
+// float g = 9.8;
+//  float yx = -(g*x*x)/2 + x ; 
+// printf("Enter x : ");
+// scanf("%d",&x);
+
+//     if (yx > 50 || yx < 30) {
+//         printf("The bullet got out\n");
+//     } else if (yx <= 50 && yx >= 30) {
+//         printf("The bullet got a goal\n");
+//     }
+
+//     return 0;
+// }
+
+
+    
+
+    
+
+
