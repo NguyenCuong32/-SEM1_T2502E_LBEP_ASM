1b4314188ed8aa4b983acbb7c678a4b21b58a0e8
heki172 <heki.17291@gmail.com>
2025-03-12 21:00:58 +0700

    phanthingochuyen

--- Bai2.1/main.c
+++ Bai2.1/main.c
@@ -13,21 +13,37 @@
 //         phan_a();
 //         return 0;
 //     }
-    //USING LOOP "WHILE"
-    #include<stdio.h>
-    void phan_b()
+    // USING LOOP "WHILE"
+    // #include<stdio.h>
+    // void phan_b()
+    // {
+    //     int i=0;
+    //     int n=6;
+    //     int element[6] ={7,6,3,34,10,710};
+    //     while (i<n)
+    //     {
+    //         printf("The element %d of the array is %d\n", i + 1, element[i]);
+    //         i++;   
+    //     }
+    // }
+    //     int main(){
+    //     phan_b();
+    //     return 0;
+    // }
+    //c) using loop "do while"
+    #include <stdio.h>
+    void phan_c()
     {
         int i=0;
         int n=6;
         int element[6] ={7,6,3,34,10,710};
-        while (i<n)
-        {
-            printf("The element %d of the array is %d\n", i + 1, element[i]);
-            i++;   
+        do{
+            printf("The element %d of the array is %d\r\n", i+1, element[i]);
+            i++;
         }
+        while (i<n);
     }
-        int main(){
-        phan_b();
+       int main(){
+        phan_c();
         return 0;
-    }
-    
+     }
